c873bd47a2d89317dce39e059e19c87ae0e191d4
hassanSaeedHassan <34820626+hassanSaeedHassan@users.noreply.github.com>
2025-05-11 22:59:54 +0300

    Delete scripts directory

--- scripts/config/__init__.py
+++
@@ -1,7 +0,0 @@
-from .company_prompts import *
-from .individual_prompts import *
-from .prompts import *
-from .prompts_legal import *
-from .bank_documents_prompts import *
-from .property_prompts import *
-from .poa_prompts import *
--- scripts/unifiers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .company_unifiers import *
-from .deeds_unifiers import *
-from .bank_unifiers import *
-from .properties_unifiers import *
